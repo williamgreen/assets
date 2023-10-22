4a8eda830e61287beb5a1d91f889c96a12e99f66
Will Green <willgreen@skiff.com>
2023-10-23 00:33:24 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import prisma from "./db";
+import { TodoItem } from "./components/TodoItem";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+  // await prisma.todo.create({ data: { title: "text", complete: false } });
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </>
+  );
+}
